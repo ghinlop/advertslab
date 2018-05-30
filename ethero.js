0d1da9ba94940b6f95c0b2d0af7a498d5f3e4101
Ghin Lop <ghinlop@gmail.com>
2018-05-30 08:40:15 +0700

    Update Ethero Auto Tool v0.2 - beta

--- ethero.js
+++ ethero.js
@@ -1,36 +1,54 @@
 $(document).ready(function () {
-    const getAllAd = $('.adspage a');
-    let AddID_arr = [];
-    for (let i = 0; i < getAllAd.length; i++) {
-        let data = {
-            _url: '',
-            _id: ''
-        };
-        let check_ad = $(getAllAd[i]).find('.hap')[0].classList.length
-        if (check_ad === 1) {
-            data._url = $(getAllAd[i]).attr('href');
-            data._id = $(getAllAd[i]).find('.hap')[0].id
-            AddID_arr.push(data)
-        }
-    }
-
+    startit = 1
+    control = 1
     let _url = document.location.href.split('.net/')[1]
     if (_url === 'ads') {
-        document.location.href = AddID_arr[0]._url
+        const getAllAd = $('.adspage a');
+        let AddID_arr = [];
+        let point_total, 
+        ad_total = getAllAd.length;
+        if(getAllAd.length > 0){
+            for (let i = 0; i < getAllAd.length; i++) {
+                let data = {
+                    _url: '',
+                    _id: ''
+                };
+                let check_ad = $(getAllAd[i]).find('.hap')[0].classList.length
+                if (check_ad === 1) {
+                    data._url = $(getAllAd[i]).attr('href');
+                    data._id = $(getAllAd[i]).find('.hap')[0].id
+                    AddID_arr.push(data)
+                }
+                point_total += $(getAllAd[i]).find('.shap').text().split(" " )[0]
+            }
+            console.log(ad_total, point_total)
+            document.location.href = AddID_arr[0]._url
+        }else{
+            setTimeout(function(){
+                location.reload();
+            }, 60000)
+        }
+        
     } else {
         $('iframe').remove()
         let _url = document.location.href.split('?ad=')[1].split('&mask=')[0]
         let loopgetAdd = setInterval(function () {
-            let result = $.ajax({
-                type: "GET",
-                url: `http://ethero.net/modules/virtual_core.php?adv=${_url}&action=adv`,
-                async: false
-            }).responseText;
-            console.log(`Đang xem ${_url} - Tình Trạng: ${result}`)
-            if ( result === "Completed") {
-                console.log(`Thành công!`)
-                document.location.href = 'http://ethero.net/ads'
+            if (view == settimeout) {
+                let result = $.ajax({
+                    type: "GET",
+                    url: `http://ethero.net/modules/virtual_core.php?adv=${_url}&action=adv`,
+                    async: false
+                }).responseText;
+                $('#desc').html(result)
+                if (result === "Completed") {
+                    document.location.href = 'http://ethero.net/ads'
+                }
+            } else {
+                view = view + 1000;
+                var width = (view * 100) / settimeout;
+                $('#barload').animate({ width: width + '%' });
+                $('#desc').html(width + "%")
             }
-        }, 10000)
+        }, 1000)
     }
 })
